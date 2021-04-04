1956f0968cf8056adc7e447204e33a5f733a631a
Ewa Potorecka <ewa.potorecka@gmail.com>
2021-04-04 16:37:23 +0200

    Game engine implemented, added tests.

--- src/engine/Game.test.ts
+++ src/engine/Game.test.ts
@@ -5,5 +5,99 @@ describe( 'Game', () => {
 		it( 'should create a game instance', () => {
 			expect( new Game() ).toBeInstanceOf( Game );
 		} );
+
+		it( 'should create empty board', () => {
+			const currentGame = new Game();
+
+			expect( currentGame.currentBoard ).toHaveLength( 9 );
+			for ( let i = 0; i < currentGame.currentBoard.length; i++ ) {
+				expect( currentGame.currentBoard[ i ] ).toEqual( 0 );
+			}
+		} );
+
+		it( 'should create history with one element - empty board', () => {
+			const currentGame = new Game();
+
+			expect( currentGame.getHistory() ).toHaveLength( 1 );
+			expect( currentGame.getHistory()[ 0 ] ).toEqual( currentGame.currentBoard );
+		} );
+	} );
+
+	describe( 'applyMove()', () => {
+		it( 'should apply the move of the player onto current board', () => {
+			const currentGame = new Game();
+
+			currentGame.applyMove( 1, 3 );
+
+			expect( currentGame.currentBoard[ 3 ] ).toEqual( 1 );
+			expect( currentGame.currentBoard[ 0 ] ).toEqual( 0 );
+		} );
+	} )
+
+	describe( 'getAvailableMoves()', () => {
+		it( 'should return an array with all of the possible moves', () => {
+			const currentGame = new Game();
+
+			expect( currentGame.getAvailableMoves() ).toHaveLength( 9 );
+
+			currentGame.applyMove( 1, 3 );
+
+			expect( currentGame.getAvailableMoves() ).toHaveLength( 8 );
+			expect( currentGame.getAvailableMoves() ).not.toContain( 3 );
+		} );
+	} );
+
+	describe( 'getResult()', () => {
+		it( 'should return PENDING, while there are still empty fields on board', () => {
+			const currentGame = new Game();
+
+			currentGame.applyMove( 1, 3 );
+
+			expect( currentGame.getResult() ).toEqual( 3 );
+		} );
+
+		it( 'should return DRAW, when all of the fields are occupied and there is no winner', () => {
+			const currentGame = new Game();
+
+			currentGame.applyMove( 1, 0 );
+			currentGame.applyMove( 1, 1 );
+			currentGame.applyMove( 2, 2 );
+			currentGame.applyMove( 2, 3 );
+			currentGame.applyMove( 2, 4 );
+			currentGame.applyMove( 1, 5 );
+			currentGame.applyMove( 1, 6 );
+			currentGame.applyMove( 2, 7 );
+			currentGame.applyMove( 1, 8 );
+
+			expect( currentGame.getResult() ).toEqual( 2 );
+		} );
+
+		it( 'should return FIRST_PLAYER_WIN, when player 1 wins', () => {
+			const currentGame = new Game();
+
+			currentGame.applyMove( 1, 0 );
+			currentGame.applyMove( 1, 1 );
+			currentGame.applyMove( 1, 2 );
+			currentGame.applyMove( 2, 3 );
+			currentGame.applyMove( 2, 4 );
+			currentGame.applyMove( 1, 5 );
+			currentGame.applyMove( 2, 7 );
+
+			expect( currentGame.getResult() ).toEqual( 0 );
+		} );
+
+		it( 'should return SECOND_PLAYER_WIN, when player 1 wins', () => {
+			const currentGame = new Game();
+
+			currentGame.applyMove( 2, 0 );
+			currentGame.applyMove( 2, 1 );
+			currentGame.applyMove( 2, 2 );
+			currentGame.applyMove( 1, 3 );
+			currentGame.applyMove( 1, 4 );
+			currentGame.applyMove( 2, 5 );
+			currentGame.applyMove( 1, 7 );
+
+			expect( currentGame.getResult() ).toEqual( 1 );
+		} );
 	} );
 } );
--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,17 +1,117 @@
+enum Results {
+	FIRST_PLAYER_WIN,
+	SECOND_PLAYER_WIN,
+	DRAW,
+	PENDING
+}
+
 export default class Game {
-	public getAvailableMoves() {
+	currentBoard: number[];
+	history: number[][];
 
+	constructor() {
+		this.history = [];
+		this.currentBoard = this.createEmptyBoard();
 	}
 
-	public getResult() {
-		
+	public getAvailableMoves(): number[] {
+		const availableMoves: number[] = [];
+
+		for ( let x = 0; x < this.currentBoard.length; x++ ) {
+			if ( this.currentBoard[ x ] === 0 ) {
+				availableMoves.push( x );
+			}
+		}
+
+		return availableMoves;
 	}
 
-	public applyMove() {
+	public getResult(): Results {
+		if ( this.currentBoard[ 0 ] === this.currentBoard[ 1 ] && this.currentBoard[ 1 ] === this.currentBoard[ 2 ] ) {
+			if ( this.currentBoard[ 0 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 3 ] === this.currentBoard[ 4 ] && this.currentBoard[ 4 ] === this.currentBoard[ 5 ] ) {
+			if ( this.currentBoard[ 3 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 6 ] === this.currentBoard[ 7 ] && this.currentBoard[ 7 ] === this.currentBoard[ 8 ] ) {
+			if ( this.currentBoard[ 6 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 0 ] === this.currentBoard[ 3 ] && this.currentBoard[ 3 ] === this.currentBoard[ 6 ] ) {
+			if ( this.currentBoard[ 0 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 1 ] === this.currentBoard[ 4 ] && this.currentBoard[ 4 ] === this.currentBoard[ 7 ] ) {
+			if ( this.currentBoard[ 1 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 2 ] === this.currentBoard[ 5 ] && this.currentBoard[ 5 ] === this.currentBoard[ 8 ] ) {
+			if ( this.currentBoard[ 2 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 0 ] === this.currentBoard[ 4 ] && this.currentBoard[ 4 ] === this.currentBoard[ 8 ] ) {
+			if ( this.currentBoard[ 0 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard[ 2 ] === this.currentBoard[ 4 ] && this.currentBoard[ 4 ] === this.currentBoard[ 6 ] ) {
+			if ( this.currentBoard[ 2 ] === 1 ) {
+				return Results.FIRST_PLAYER_WIN
+			} else if ( this.currentBoard[ 0 ] === 2 ) {
+				return Results.SECOND_PLAYER_WIN;
+			}
+		}
+		if ( this.currentBoard.includes( 0 ) ) {
+			return Results.PENDING;
+		}
 
+		return Results.DRAW;
 	}
 
-	public getHistory() {
+	public applyMove( player: 1 | 2, move: number ): void {
+		this.currentBoard[ move ] = player;
+		this.history.push( this.currentBoard );
+	}
 
+	public getHistory(): number[][] {
+		return this.history;
+	}
+
+	public printCurrentBoard(): void {
+		console.log( this.currentBoard );
+	}
+
+	private createEmptyBoard(): number[] {
+		const board: number[] = [];
+
+		for ( let x = 0; x < 9; x++ ) {
+			board.push( 0 );
+		}
+
+		this.history.push( board );
+
+		return board;
 	}
 }
